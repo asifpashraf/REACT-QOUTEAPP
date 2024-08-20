ffec74522a2a82d251f02be34f786f7ad62067f0
asif p ashraf <asifpashraf@gmail.com>
2024-08-20 18:30:08 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import Quotes from "./Qoutes"
+import nightsky from './assets/nightsky.jpg'
+import './App.css'
 
 
 function App(){
@@ -10,7 +12,7 @@ function App(){
   function category(event){
     let cat =event.target.value
     setAuthor(cat)
-}
+    }
 function random(){
  const randomIndex =Math.floor(Math.random()*filterdQoutes.length)
  const randomQoute=filterdQoutes[randomIndex].quote
@@ -23,19 +25,19 @@ function random(){
   
   
   return (<>
-  <div className="flex flex-col justify-center items-center h-[100vh] bg-orange-300">
-    <select name="" id="" onChange={category}>
+  <div className="flex flex-col justify-center items-center h-[100vh] bg-orange-300 page" >
+    <select name="" id="" onChange={category} className="bg-orange-200 text-black">
       <option value="Albert Einstein">Albert Einstein</option>
       <option value="Maya Angelou">Maya Angelou</option>
       <option value="Steve Jobs">Steve Jobs</option>
       <option value="Mahatma Gandhi">Mahatma Gandhi</option>
       <option value="Oscar Wilde">Oscar Wilde</option>
     </select>
-    <h2>{auhtor}</h2>
-    <h1>{q}</h1>
+    <h2 className="text-white">{auhtor}</h2>
+    <h1 className="text-white">{q}</h1>
     
     
-    <button onClick={random}>start your day</button>
+    <button onClick={random} className="text-white border-2 rounded-2xl w-[200px] bg-orange-700">start your day</button>
     
   </div>
   </>)
